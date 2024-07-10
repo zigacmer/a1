ce4d627fb2a6eaa816e8d86b437394e110690968
ziga.cmer <ziga.cmer@gmail.com>
2024-07-10 22:08:41 +0000

    updates in images and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic":"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundImage: {
+        'hero-pattern': "url('public/images/hero-pattern.png')",
       },
       colors: {
         'footer-green': '#074236',
